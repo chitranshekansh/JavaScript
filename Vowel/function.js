610eb0580cc93dc58fb1a41baa58ffbf69e19fa7
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-02-05 21:06:45 +0530

    Add files via upload

---
+++ Vowel/function.js
@@ -0,0 +1,8 @@
+function vowel()
+{
+    var vow=document.getElementById("char").value;
+    if(vow=='a'||vow=='e'||vow=='i'||vow=='o'||vow=='u'||vow=='A'||vow=='E'||vow=='I'||vow=='O'||vow=='U')
+    alert("Character is Vowel");
+    else
+    alert("Not a Vowel")
+}
