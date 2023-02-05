dac1dc661017a3bd569039be507590bc649bddc3
Ekansh Arun Srivastava <104249396+chitranshekansh@users.noreply.github.com>
2023-02-05 20:32:38 +0530

    Add files via upload

---
+++ GreaterTwo/greater.js
@@ -0,0 +1,12 @@
+function greater()
+{
+    var a=document.getElementById("num1").value;
+    var b=document.getElementById("num2").value;
+    
+    if(a>b)
+    alert(a+" is greater");
+    else
+    alert(b+" is greater");
+
+    
+}
